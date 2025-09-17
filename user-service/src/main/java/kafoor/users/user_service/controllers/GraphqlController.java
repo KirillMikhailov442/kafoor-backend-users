a91d3ee974eb7a901f20788adaffccc59ab18500
Kirill Mikhailov <137108844+KirillMikhailov442@users.noreply.github.com>
2025-09-17 22:54:07 +0500

    fix: added queries with roles to the GraphQL

--- user-service/src/main/java/kafoor/users/user_service/controllers/GraphqlController.java
+++ user-service/src/main/java/kafoor/users/user_service/controllers/GraphqlController.java
@@ -1,7 +1,11 @@
 package kafoor.users.user_service.controllers;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import kafoor.users.user_service.models.Role;
+import kafoor.users.user_service.models.Token;
 import kafoor.users.user_service.models.User;
+import kafoor.users.user_service.services.RoleService;
+import kafoor.users.user_service.services.TokenService;
 import kafoor.users.user_service.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
@@ -10,11 +14,15 @@ import org.springframework.stereotype.Controller;
 
 import java.util.List;
 
-@Tag(name = "User-GraphQL", description = "GraphQL operations for user management")
+@Tag(name = "GraphQL", description = "GraphQL operations")
 @Controller
-public class UserGraphqlController {
+public class GraphqlController {
     @Autowired
     private UserService userService;
+    @Autowired
+    private RoleService roleService;
+    @Autowired
+    private TokenService tokenService;
 
     @QueryMapping
     public List<User> getAllUsers(){
@@ -24,5 +32,20 @@ public class UserGraphqlController {
     @QueryMapping
     public User getUserById(@Argument Long id){
         return userService.findUserById(id);
+    }
+
+    @QueryMapping
+    public List<Role> getAllRoles(){
+        return roleService.getAllRoles();
+    }
+
+    @QueryMapping
+    public Role getRoleById(@Argument Long id){
+        return roleService.findRoleById(id);
+    }
+
+    @QueryMapping
+    public Token getTokenById(@Argument Long id){
+        return tokenService.findTokenById(id);
     }
 }
