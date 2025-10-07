f1a84a91c13fba0e498efc452b7fb2b1a69332d3
Kirill Mikhailov <137108844+KirillMikhailov442@users.noreply.github.com>
2025-10-08 01:27:26 +0500

    fix: maxMembers changed

--- src/main/java/kafoor/users/user_service/services/UserService.java
+++ src/main/java/kafoor/users/user_service/services/UserService.java
@@ -9,7 +9,6 @@ import kafoor.users.user_service.models.Token;
 import kafoor.users.user_service.models.User;
 import kafoor.users.user_service.models.enums.Tokens;
 import kafoor.users.user_service.models.enums.UserRoles;
-import kafoor.users.user_service.repositories.TokenRepo;
 import kafoor.users.user_service.repositories.UserRepo;
 import kafoor.users.user_service.utils.jwt.JWTUtils;
 import kafoor.users.user_service.utils.jwt.UserPrincipal;
@@ -39,34 +38,37 @@ public class UserService implements UserDetailsService {
     private JWTUtils jwtUtils;
     private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
-    public List<User> getAllUsers(){
+    public List<User> getAllUsers() {
         List<User> users = userRepo.findAll();
-        if(users.isEmpty()) throw new NotFound("Users not found");
+        if (users.isEmpty())
+            throw new NotFound("Users not found");
         return users;
     }
 
-    public User findUserById(long id){
+    public User findUserById(long id) {
         return userRepo.findById(id).orElseThrow(() -> new NotFound("User not found by id"));
     }
 
-    public User findUserByNickname(String nickname){
+    public User findUserByNickname(String nickname) {
         return userRepo.findByNickname(nickname).orElseThrow(() -> new NotFound(("User not found by nickname")));
     }
 
-    public User findUserByEmail(String email){
-        return userRepo.findByEmail(email).orElseThrow(()-> new NotFound("User not found by email"));
+    public User findUserByEmail(String email) {
+        return userRepo.findByEmail(email).orElseThrow(() -> new NotFound("User not found by email"));
     }
 
-    public void confirmUser(long id){
+    public void confirmUser(long id) {
         User user = findUserById(id);
         user.setConfirmed(true);
         userRepo.save(user);
     }
 
     @Transient
-    public UserCreateResDTO createUser(UserCreateReqDTO userDto, HeaderDTO headerDto){
-        if(userRepo.existsByEmail(userDto.getEmail())) throw new Conflict(("A user with such email already exists"));
-        if(userRepo.existsByNickname(userDto.getNickname())) throw new Conflict(("A user with such nickname already exists"));
+    public UserCreateResDTO createUser(UserCreateReqDTO userDto, HeaderDTO headerDto) {
+        if (userRepo.existsByEmail(userDto.getEmail()))
+            throw new Conflict(("A user with such email already exists"));
+        if (userRepo.existsByNickname(userDto.getNickname()))
+            throw new Conflict(("A user with such nickname already exists"));
 
         Set<Role> roles = Set.of(roleService.findOrCreateRole(UserRoles.USER));
         User newUser = User.builder()
@@ -93,48 +95,53 @@ public class UserService implements UserDetailsService {
                 .refreshToken(tokenRefresh).build();
     }
 
-    public User updateUser(UserUpdateDTO dto, long id){
+    public User updateUser(UserUpdateDTO dto, long id) {
         User user = findUserById(id);
-        if(userRepo.existsByEmail(dto.getEmail()) && user.getId() != id){
+        if (userRepo.existsByEmail(dto.getEmail()) && user.getId() != id) {
             throw new Conflict("A user with such email already exists");
         }
-        if(userRepo.existsByNickname(dto.getNickname()) && user.getId() != id){
+        if (userRepo.existsByNickname(dto.getNickname()) && user.getId() != id) {
             throw new Conflict("A user with such nickname already exists");
         }
-        if(dto.getName() != null && !dto.getName().isEmpty()) user.setName(dto.getName());
-        if(dto.getEmail() != null && !dto.getEmail().isEmpty()) user.setEmail(dto.getEmail());
-        if(dto.getNickname() != null && !dto.getNickname().isEmpty()) user.setNickname(dto.getNickname());
+        if (dto.getName() != null && !dto.getName().isEmpty())
+            user.setName(dto.getName());
+        if (dto.getEmail() != null && !dto.getEmail().isEmpty())
+            user.setEmail(dto.getEmail());
+        if (dto.getNickname() != null && !dto.getNickname().isEmpty())
+            user.setNickname(dto.getNickname());
 
         return userRepo.save(user);
     }
 
-    public void deleteUser(long id){
+    public void deleteUser(long id) {
         userRepo.deleteById(id);
     }
 
-    public void passwordChangeOfUser(PasswordChangeDTO dto, long id){
+    public void passwordChangeOfUser(PasswordChangeDTO dto, long id) {
         User user = findUserById(id);
-        if(!encoder.matches(dto.getCurrentPassword(), user.getPassword())) throw new Conflict("Current password does not match user password");
+        if (!encoder.matches(dto.getCurrentPassword(), user.getPassword()))
+            throw new Conflict("Current password does not match user password");
         user.setPassword(encoder.encode(dto.getNewPassword()));
         userRepo.save(user);
     }
 
     @Transient
-    public TokensDTO login(LoginDTO dto, HeaderDTO headerDTO){
+    public TokensDTO login(LoginDTO dto, HeaderDTO headerDTO) {
         User user = findUserByEmail(dto.getEmail());
-        if(!encoder.matches(dto.getPassword(), user.getPassword())) throw new BadRequest("Incorrect password");
+        if (!encoder.matches(dto.getPassword(), user.getPassword()))
+            throw new BadRequest("Incorrect password");
         UserPrincipal userPrincipal = new UserPrincipal(user);
         Token token = null;
-       try{
-           token = tokenService.findTokenByUserAndUserAgentAndIP(user, headerDTO.getUserAgent(), headerDTO.getIP());
-       }catch (NotFound notFound){
-           String tokenRefresh = jwtUtils.generateToken(userPrincipal, Tokens.REFRESH_TOKEN);
-           TokenCreateDTO tokenCreateDTO = TokenCreateDTO.builder()
-                   .IP(headerDTO.getIP())
-                   .userAgent(headerDTO.getUserAgent())
-                   .refresh(tokenRefresh).build();
-           token = tokenService.createToken(tokenCreateDTO, user);
-       }
+        try {
+            token = tokenService.findTokenByUserAndUserAgentAndIP(user, headerDTO.getUserAgent(), headerDTO.getIP());
+        } catch (NotFound notFound) {
+            String tokenRefresh = jwtUtils.generateToken(userPrincipal, Tokens.REFRESH_TOKEN);
+            TokenCreateDTO tokenCreateDTO = TokenCreateDTO.builder()
+                    .IP(headerDTO.getIP())
+                    .userAgent(headerDTO.getUserAgent())
+                    .refresh(tokenRefresh).build();
+            token = tokenService.createToken(tokenCreateDTO, user);
+        }
 
         Map<String, Object> userClaims = new HashMap<>();
         userClaims.put("roles", userPrincipal.getAuthorities());
@@ -147,9 +154,9 @@ public class UserService implements UserDetailsService {
                 .refreshToken(refreshToken).build();
     }
 
-    public TokensDTO updateRefreshTokenOfUser(TokenCreateDTO dto){
+    public TokensDTO updateRefreshTokenOfUser(TokenCreateDTO dto) {
         String currentRefresh = dto.getRefresh();
-        if(!jwtUtils.isTokenExpired(currentRefresh, Tokens.REFRESH_TOKEN)){
+        if (!jwtUtils.isTokenExpired(currentRefresh, Tokens.REFRESH_TOKEN)) {
             throw new BadRequest("Token expired");
         }
         Token token = tokenService.findTokenByRefresh(currentRefresh);
@@ -165,11 +172,13 @@ public class UserService implements UserDetailsService {
         return TokensDTO.builder().accessToken(accessToken).refreshToken(refreshToken).build();
     }
 
-    public void addRole(UserRoles newRole, long id){
+    public void addRole(UserRoles newRole, long id) {
         User user = findUserById(id);
         boolean hasRole = user.getRoles().stream()
                 .anyMatch(role -> role.equals(newRole));
-        if(hasRole) throw new Conflict(String.format("The role of %s has already been issued to this user", newRole.toString()));
+        if (hasRole)
+            throw new Conflict(
+                    String.format("The role of %s has already been issued to this user", newRole.toString()));
         Set<Role> roles = user.getRoles();
         Role newRoleEntity = roleService.findOrCreateRole(newRole);
         roles.add(newRoleEntity);
@@ -177,23 +186,24 @@ public class UserService implements UserDetailsService {
         userRepo.save(user);
     }
 
-    public void removeRole(UserRoles currentRole, long id){
+    public void removeRole(UserRoles currentRole, long id) {
         User user = findUserById(id);
         boolean hasRole = user.getRoles().stream()
                 .anyMatch(role -> role.getName().equals(currentRole));
-        if(!hasRole) throw new Conflict(String.format("The role of the %s is not of this role", currentRole.toString()));
+        if (!hasRole)
+            throw new Conflict(String.format("The role of the %s is not of this role", currentRole.toString()));
         Role removeRole = roleService.findRoleByName(currentRole);
         user.getRoles().remove(removeRole);
         userRepo.save(user);
     }
 
-    public void banUser(long id){
+    public void banUser(long id) {
         User user = findUserById(id);
         user.setDeactivatedAt(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
         userRepo.save(user);
     }
 
-    public void unbanUser(long id){
+    public void unbanUser(long id) {
         User user = findUserById(id);
         user.setDeactivatedAt(null);
         userRepo.save(user);
@@ -202,7 +212,7 @@ public class UserService implements UserDetailsService {
     @Override
     public UserPrincipal loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepo.findById(Long.valueOf(username))
-                .orElseThrow(()-> new UsernameNotFoundException(username));
+                .orElseThrow(() -> new UsernameNotFoundException(username));
         return new UserPrincipal(user);
     }
 }
